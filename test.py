63b739eead95de42d100a2d4aed02a193ae0c939
unknown <lucas.pohl01@gmail.com>
2024-07-22 18:40:58 +0200

    Aufgeräumt

--- test.py
+++ test.py
@@ -2,11 +2,4 @@ print("Hello World")
 print("Penis") 
 print("wo")
 print("FCS")
-
-<<<<<<< HEAD
-
-
 print("Fick dieses scheiß Fach")
-=======
-print("leck jetzt eier")
->>>>>>> 1f8621979cb2571a49b05fbe1ce9c533751b861e
