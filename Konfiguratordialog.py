2466feadd50cc505484c992f00a7c358bbb405cd
Jakob <jakobbauer4@t-online.de>
2024-08-27 14:26:57 +0200

    Addition

--- Konfiguratordialog.py
+++ Konfiguratordialog.py
@@ -2,23 +2,26 @@ from consolemenu import*                                #Console Menu Importiere
 from consolemenu.items import*    
 from Funktionen import Ausgabe_nach_ID
 from Funktionen import Ausgabe_nach_Preis
+from Funktionen import Platzhalter
 
 main_menu = ConsoleMenu("htw Autokonfigurator", "Wählen Sie eine Option")
 
 submenu1 = ConsoleMenu("Ausstattungsoptionen", "Wählen Sie eine Option")
 submenu2 = ConsoleMenu("Konfiguration starten","Wählen Sie eine Option")
+submenu3 = ConsoleMenu("Farbe","Wählen Sie eine Option",submenu2)
 
 submenu1_item1 = FunctionItem("Zeige alle Austattungsoptionen an", Ausgabe_nach_ID)
 submenu1_item2 = FunctionItem("Zeige alle Austattungsoptionen nach Preis aufsteigend an", Ausgabe_nach_Preis)
 submenu1.append_item(submenu1_item1)
 submenu1.append_item(submenu1_item2)
 
-submenu2_item1 = SubmenuItem("Farbe", submenu2)
-submenu2_item2 = SubmenuItem("Innenraum", submenu2 )
-submenu2_item3 = SubmenuItem("Exterieur",submenu2)
-submenu2_item4 = SubmenuItem("Extras",submenu2)
-submenu2_item5 = SubmenuItem("Infotainment",submenu2)
-submenu2_item6 = SubmenuItem("Felgen",submenu2)
+submenu2_item1 = SubmenuItem("Modell", submenu2)
+submenu2_item2 = SubmenuItem("Farbe", submenu2 )
+submenu2_item3 = SubmenuItem("Felgen",submenu2)
+submenu2_item4 = SubmenuItem("Interieur",submenu2)
+submenu2_item5 = SubmenuItem("Exterieur",submenu2)
+submenu2_item6 = SubmenuItem("Infotainment",submenu2)
+submenu2_item7 = SubmenuItem("Extras",submenu2)
 submenu2.append_item(submenu2_item1)
 submenu2.append_item(submenu2_item2)
 submenu2.append_item(submenu2_item3)
@@ -26,8 +29,14 @@ submenu2.append_item(submenu2_item4)
 submenu2.append_item(submenu2_item5)
 submenu2.append_item(submenu2_item6)
 
-main_menu.append_item(SubmenuItem("Austattungsoptionen", submenu1, main_menu))
+submenu3_item1 = FunctionItem("Blau", Platzhalter)
+submenu3_item2 = FunctionItem("Rot", Platzhalter)
+submenu3.append_item(submenu3_item1)
+submenu3.append_item(submenu3_item2)
 
+
+main_menu.append_item(SubmenuItem("Austattungsoptionen", submenu1, main_menu))
+main_menu.append_item(SubmenuItem("Konfiguration starten", submenu2, main_menu))
 main_menu.show()
 
 
